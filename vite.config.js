cc70b3e9bef229c1752214559fc71642e93f2662
lindani <panilindani02@gmail.com>
2026-05-02 07:37:44 +0200

    Feat: rector app with vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Fixes the "motion/react" not found error
+      "motion/react": "framer-motion",
+      // Allows you to use @/utils/cn instead of ../../utils/cn if you want
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
